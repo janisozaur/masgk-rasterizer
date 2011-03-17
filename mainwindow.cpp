3d984aea605b457642f499e3dd47fc63ec50480a
Michał Janiszewski <janisozaur@gmail.com>
2011-03-18 00:26:40 +0100

    add color rendering

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,9 +9,9 @@ MainWindow::MainWindow(QWidget *parent) :
 	ui(new Ui::MainWindow)
 {
 	Rasterizer *raster = new Rasterizer(512, 512);
-	ColorVertex c1(QVector3D(20, 20, 20), Qt::white);
-	ColorVertex c2(QVector3D(30, 40, 20), Qt::white);
-	ColorVertex c3(QVector3D(40, 20, 20), Qt::white);
+	ColorVertex c1(QVector3D(20, 120, 20), Qt::red);
+	ColorVertex c2(QVector3D(130, 60, 20), Qt::green);
+	ColorVertex c3(QVector3D(40, 20, 20), Qt::blue);
 	raster->triangle(c1, c2, c3);
 	raster->getColorBuffer().save("out.png");
 	ui->setupUi(this);
--- rasterizer.cpp
+++ rasterizer.cpp
@@ -20,26 +20,32 @@ void Rasterizer::vertex(QVector3D v)
 
 void Rasterizer::triangle(ColorVertex a, ColorVertex b, ColorVertex c)
 {
-	const int y1 = a.mPos.y();
-	const int y2 = b.mPos.y();
-	const int y3 = c.mPos.y();
+	const float y1 = a.mPos.y();
+	const float y2 = b.mPos.y();
+	const float y3 = c.mPos.y();
 
-	const int x1 = a.mPos.x();
-	const int x2 = b.mPos.x();
-	const int x3 = c.mPos.x();
+	const float x1 = a.mPos.x();
+	const float x2 = b.mPos.x();
+	const float x3 = c.mPos.x();
 
-	const int minx = qMin(x1, qMin(x2, x3));
-	const int maxx = qMax(x1, qMax(x2, x3));
-	const int miny = qMin(y1, qMin(y2, y3));
-	const int maxy = qMax(y1, qMax(y2, y3));
+	const int minx = (qMin(x1, qMin(x2, x3)));
+	const int maxx = (qMax(x1, qMax(x2, x3)));
+	const int miny = (qMin(y1, qMin(y2, y3)));
+	const int maxy = (qMax(y1, qMax(y2, y3)));
 
-	for (int i = miny; i < maxy; i++) {
-		for (int j = minx; j < maxx; j++) {
-			if ((x1 - x2) * (i - y1) - (y1 - y2) * (j - x1) > 0 &&
-				(x2 - x3) * (i - y2) - (y2 - y3) * (j - x2) > 0 &&
-				(x3 - x1) * (i - y3) - (y3 - y1) * (j - x3) > 0)
+	for (int y = miny; y < maxy; y++) {
+		for (int x = minx; x < maxx; x++) {
+			if ((x1 - x2) * (y - y1) - (y1 - y2) * (x - x1) > 0 &&
+				(x2 - x3) * (y - y2) - (y2 - y3) * (x - x2) > 0 &&
+				(x3 - x1) * (y - y3) - (y3 - y1) * (x - x3) > 0)
 			{
-				mColorBuffer.setPixel(i, j, mPaintColor.rgba());
+				float l1 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
+				float l2 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / ((y3 - y1) * (x2 - x3) + (x1 - x3) * (y2 - y3));
+				float l3 = 1 - l1 - l2;
+				int red = l1 * a.mColor.red() + l2 * b.mColor.red() + l3 * c.mColor.red();
+				int green = l1 * a.mColor.green() + l2 * b.mColor.green() + l3 * c.mColor.green();
+				int blue = l1 * a.mColor.blue() + l2 * b.mColor.blue() + l3 * c.mColor.blue();
+				mColorBuffer.setPixel(y, x, QColor(red, green, blue).rgb());
 			}
 		}
 	}
