3fe8264fe959f0d2874aa33a85965bd336168475
Michał Janiszewski <janisozaur@gmail.com>
2011-03-30 01:41:06 +0200

    add gui to show changes. it should also support future scripting and saving capabilities

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,17 +4,21 @@
 #include "rasterizer.h"
 #include "colorvertex.h"
 
+#include <QPixmap>
+
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
 {
+	ui->setupUi(this);
+
 	Rasterizer *raster = new Rasterizer(512, 512);
 	ColorVertex c1(QVector3D(20, 120, 20), Qt::red);
 	ColorVertex c2(QVector3D(130, 60, 20), Qt::green);
 	ColorVertex c3(QVector3D(40, 20, 20), Qt::blue);
 	raster->triangle(c1, c2, c3);
-	raster->getColorBuffer().save("out.png");
-	ui->setupUi(this);
+	QImage result = raster->getColorBuffer();
+	ui->outputDisplayLabel->setPixmap(QPixmap::fromImage(result));
 }
 
 MainWindow::~MainWindow()
