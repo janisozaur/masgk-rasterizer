0badedd8d30b5b91adc159679cd60d7b19ca07cf
Michał Janiszewski <janisozaur@gmail.com>
2011-04-08 00:26:31 +0200

    fix coordinate system (x+ to right, y+ upwards, z+ to user), use the new color (not yet working), fix normal buffer

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -64,11 +64,12 @@ void Rasterizer::triangle(ColorVertex a, ColorVertex b, ColorVertex c)
 
 	for (int y = miny; y < maxy; y++) {
 		for (int x = minx; x < maxx; x++) {
+			QPoint pos(x, mColorBuffer.height() - y - 1);
 			qreal l1 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
 			qreal l2 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / ((y3 - y1) * (x2 - x3) + (x1 - x3) * (y2 - y3));
 			qreal l3 = 1 - l1 - l2;
 			int d = a.mPos.z() * l1 + b.mPos.z() * l2 + c.mPos.z() * l3;
-			if ((mDepthBuffer.pixelIndex(x, y) > d) &&
+			if ((mDepthBuffer.pixelIndex(pos) > d) &&
 				(qBound(0, d, 255) == d) &&
 				(x1 - x2) * (y - y1) - (y1 - y2) * (x - x1) > 0 &&
 				(x2 - x3) * (y - y2) - (y2 - y3) * (x - x2) > 0 &&
@@ -76,12 +77,15 @@ void Rasterizer::triangle(ColorVertex a, ColorVertex b, ColorVertex c)
 			{
 				QVector3D n = na * l1 + nb * l2 + nc * l3;
 				n.normalize();
-				int red = l1 * a.mColor.red() + l2 * b.mColor.red() + l3 * c.mColor.red();
-				int green = l1 * a.mColor.green() + l2 * b.mColor.green() + l3 * c.mColor.green();
-				int blue = l1 * a.mColor.blue() + l2 * b.mColor.blue() + l3 * c.mColor.blue();
-				mColorBuffer.setPixel(x, y, QColor(red, green, blue).rgb());
-				mDepthBuffer.setPixel(x, y, (int)d);
-				mNormalBuffer.setPixel(x, y, qRgb(n.x() * 255, n.y() * 255, n.z() * 255));
+				//int red = l1 * a.mColor.red() + l2 * b.mColor.red() + l3 * c.mColor.red();
+				//int green = l1 * a.mColor.green() + l2 * b.mColor.green() + l3 * c.mColor.green();
+				//int blue = l1 * a.mColor.blue() + l2 * b.mColor.blue() + l3 * c.mColor.blue();
+				QVector3D color = ((da + aa) * l1 + (db + ab) * l2 + (dc + ac) * l3) * 255;
+				mColorBuffer.setPixel(pos, qRgb(color.x(), color.y(), color.z()));
+				mDepthBuffer.setPixel(pos, (int)d);
+				n /= 2;
+				n += QVector3D(0.5, 0.5, 0.5);
+				mNormalBuffer.setPixel(pos, qRgb(n.x() * 255, n.y() * 255, n.z() * 255));
 			}
 		}
 	}
