daa22ff7f2d3a2cb57ef71bb087667a32b569ea9
Michał Janiszewski <janisozaur@gmail.com>
2011-04-09 23:45:08 +0200

    don't test depth for pixels that are not being rendered

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -85,31 +85,31 @@ void Rasterizer::triangle(ColorVertex a, ColorVertex b, ColorVertex c)
 	for (int y = miny; y < maxy; y++) {
 		for (int x = minx; x < maxx; x++) {
 			QPoint pos(x, mColorBuffer.height() - y - 1);
-			qreal l1 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
-			qreal l2 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / ((y3 - y1) * (x2 - x3) + (x1 - x3) * (y2 - y3));
-			qreal l3 = 1 - l1 - l2;
-			qreal d = a.mPos.z() * l1 + b.mPos.z() * l2 + c.mPos.z() * l3;
 			int idx = pos.y() * mColorBuffer.width() + pos.x();
-			if ((mDepthBuffer.at(idx) > d) &&
-				//(qBound(0, d, 255) == d) &&
-				(x1 - x2) * (y - y1) - (y1 - y2) * (x - x1) >= 0 &&
+			if  ((x1 - x2) * (y - y1) - (y1 - y2) * (x - x1) >= 0 &&
 				(x2 - x3) * (y - y2) - (y2 - y3) * (x - x2) >= 0 &&
 				(x3 - x1) * (y - y3) - (y3 - y1) * (x - x3) >= 0)
 			{
-				QVector3D n = na * l1 + nb * l2 + nc * l3;
-				n.normalize();
-				//int red = l1 * a.mColor.red() + l2 * b.mColor.red() + l3 * c.mColor.red();
-				//int green = l1 * a.mColor.green() + l2 * b.mColor.green() + l3 * c.mColor.green();
-				//int blue = l1 * a.mColor.blue() + l2 * b.mColor.blue() + l3 * c.mColor.blue();
-				QVector3D color = ((da + aa + sa) * l1 + (db + ab + sb) * l2 + (dc + ac + sc) * l3) * 255;
-				color.setX(qBound(0.0, color.x(), 255.0));
-				color.setY(qBound(0.0, color.y(), 255.0));
-				color.setZ(qBound(0.0, color.z(), 255.0));
-				mColorBuffer.setPixel(pos, qRgb(color.x(), color.y(), color.z()));
-				mDepthBuffer[idx] = d;
-				n /= 2;
-				n += QVector3D(0.5, 0.5, 0.5);
-				mNormalBuffer.setPixel(pos, qRgb(n.x() * 255, n.y() * 255, n.z() * 255));
+				qreal l1 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3));
+				qreal l2 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / ((y3 - y1) * (x2 - x3) + (x1 - x3) * (y2 - y3));
+				qreal l3 = 1 - l1 - l2;
+				qreal d = a.mPos.z() * l1 + b.mPos.z() * l2 + c.mPos.z() * l3;
+				if (mDepthBuffer.at(idx) > d) {
+					QVector3D n = na * l1 + nb * l2 + nc * l3;
+					n.normalize();
+					//int red = l1 * a.mColor.red() + l2 * b.mColor.red() + l3 * c.mColor.red();
+					//int green = l1 * a.mColor.green() + l2 * b.mColor.green() + l3 * c.mColor.green();
+					//int blue = l1 * a.mColor.blue() + l2 * b.mColor.blue() + l3 * c.mColor.blue();
+					QVector3D color = ((da + aa + sa) * l1 + (db + ab + sb) * l2 + (dc + ac + sc) * l3) * 255;
+					color.setX(qBound(0.0, color.x(), 255.0));
+					color.setY(qBound(0.0, color.y(), 255.0));
+					color.setZ(qBound(0.0, color.z(), 255.0));
+					mColorBuffer.setPixel(pos, qRgb(color.x(), color.y(), color.z()));
+					mDepthBuffer[idx] = d;
+					n /= 2;
+					n += QVector3D(0.5, 0.5, 0.5);
+					mNormalBuffer.setPixel(pos, qRgb(n.x() * 255, n.y() * 255, n.z() * 255));
+				}
 			}
 		}
 	}
